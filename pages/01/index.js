fafbbad37bc26d202f59618c335633021c8b8fc5
JayJHFE <dove92lee@gmail.com>
2022-10-31 22:39:44 +0900

    FAQ 과제

---
+++ pages/01/index.js
@@ -0,0 +1,49 @@
+import {Wrapper, Header, Menubar, Name, Photo, Profile, Glass, Arrow,SmallMenu, SmallMenuRed, BodyWrapper,Question, QuestionNumber, BottomArrow, BottomMenu, Icon, UserMenu, UserMenuRed} from '../../styles/emotion'
+
+export default function EmotionPage() {
+
+    // 여기는 자바스크립트 쓰는곳
+
+    return (
+    <div>
+        <Wrapper>
+            <Glass src='/Group.png' />
+            <Header>
+                마이
+                <Profile>
+                <Photo src='/Photo.png' />
+                <Name>임정아</Name>
+                <Arrow src='/arrow.png' />
+                </Profile>
+            </Header>
+            <Menubar>
+                <SmallMenu>공지사항</SmallMenu>
+                <SmallMenu>이벤트</SmallMenu>
+                <SmallMenuRed>FAQ</SmallMenuRed>
+                <SmallMenu>Q&A</SmallMenu>
+            </Menubar>
+            <BodyWrapper>
+                <QuestionNumber>Q. 01</QuestionNumber>
+                <Question>리뷰 작성은 어떻게 하나요? <BottomArrow src='/BottomArrow.png' /></Question>
+                <QuestionNumber>Q. 02</QuestionNumber>
+                <Question>리뷰 수정/삭제는 어떻게 하나요? <BottomArrow src='/BottomArrow.png' /></Question>
+                <QuestionNumber>Q. 03</QuestionNumber>
+                <Question>아이디/비밀번호를 잊어버렸어요 <BottomArrow src='/BottomArrow.png' /></Question>
+                <QuestionNumber>Q. 04</QuestionNumber>
+                <Question>회원탈퇴를 하고싶어요. <BottomArrow src='/BottomArrow.png' /></Question>
+                <QuestionNumber>Q. 05</QuestionNumber>
+                <Question>출발지 설정은 어떻게 하나요? <BottomArrow src='/BottomArrow.png' /></Question>
+                <QuestionNumber>Q. 06</QuestionNumber>
+                <Question>비밀번호를 변경하고 싶어요 <BottomArrow src='/BottomArrow.png' /></Question>
+            </BodyWrapper>
+            <BottomMenu>
+                <UserMenu><Icon src='/home.png' />홈</UserMenu>
+                <UserMenu><Icon src='/location.png' />잇츠로드</UserMenu>
+                <UserMenu><Icon src='/heart.png' />마이찜</UserMenu>
+                <UserMenuRed><Icon src='/person.png' />마이</UserMenuRed>    
+            </BottomMenu> 
+        </Wrapper>
+        </div>
+    )
+
+}
---
+++ styles/emotion.js
@@ -0,0 +1,129 @@
+import styled from '@emotion/styled'
+
+export const Wrapper  = styled.div`
+    width: 640px;
+    height: 1138px;
+    color: red;
+
+`
+export const Header = styled.div`
+    width: 640px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding-left: 55px;
+    color: black;
+    font-size: 40px;
+    font-weight: 700;
+    margin-top: 10px;
+`
+export const Profile = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    margin-right: 20px;
+
+`
+export const Photo = styled.img`
+    width: 60px;
+    height: 60px;
+`
+
+export const Name = styled.div`
+    font-size: 24px;
+    font-weight: 700;
+    margin-left: 20px;
+`
+
+export const Menubar = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    padding: 30px 90px 30px 30px;
+    border-bottom: 1px solid #CACACA;
+    text-align: center;
+`
+export const Glass = styled.img`
+    width: 23px;
+    height: 23px;
+    margin-left: 580px;
+    margin-top: 30px;
+`
+
+export const Arrow = styled.img`
+    width: 15px;
+    height: 15px;
+    margin-left: 20px;
+    margin-right: 15px;
+`
+
+export const SmallMenu = styled.div`
+    color: #CACACA;
+    font-size: 30px;
+    font-weight: 700;
+
+`
+
+export const SmallMenuRed = styled.div`
+    color: #CACACA;
+    font-size: 30px;
+    font-weight: 700;
+    color: #FF1B6D;
+    border-bottom: 1px solid #FF1B6D;
+
+`
+export const BodyWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 20px 30px 10px 30px;
+    border-bottom: 1px solid #CACACA;
+`
+export const Question = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 5px 0px 35px 30px ;
+    color: black;
+    font-size: 24px;
+    font-weight: 400;
+`
+
+export const QuestionNumber = styled.div`
+    font-size: 18px;
+    color: #ADADAD;
+    font-weight: 400;
+    padding-left: 30px;
+`
+
+export const BottomArrow = styled.img`
+    width: 16px;
+    height: 16px;
+    margin-right: 20px;    
+`
+export const BottomMenu = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    padding: 20px 30px 0px 30px;
+`
+
+export const UserMenu = styled.div`
+    display: flex;
+    flex-direction: column;
+    color: #ADADAD;
+    font-size: 16px;
+    text-align: center;
+`
+export const UserMenuRed = styled.div`
+    display: flex;
+    flex-direction: column;
+    color: #FF1B6D;
+    font-size: 16px;
+    text-align: center;
+`
+
+export const Icon = styled.img`
+    width: 41px;
+    height: 41px;
+    padding-left: 1px;
+`
